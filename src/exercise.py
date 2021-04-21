d19ee22813b4cc2041799dfc68c5528dc9c23990
MrSullivan <>
2021-04-21 11:53:46 +0000

    added while statement

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
 
+    userNumber = int(input(""))
+    i = 0
+
+    while (i<userNumber):
+      print(i)
+      i += 1
+
+
 if __name__ == '__main__':
     main()
